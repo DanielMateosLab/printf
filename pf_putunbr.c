a02acf75caad2550c417de8a3e2cc69553a6562a
Daniel Mateos Labrador <daniel.mateos@asafedigital.com>
2024-05-17 18:01:27 +0200

    Fix putunbr strict errors

--- pf_putunbr.c
+++ pf_putunbr.c
@@ -6,7 +6,7 @@
 /*   By: damateos <damateos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/15 21:52:27 by damateos          #+#    #+#             */
-/*   Updated: 2024/05/13 22:26:31 by damateos         ###   ########.fr       */
+/*   Updated: 2024/05/17 18:00:54 by damateos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,23 @@
 int	pf_putunbr(unsigned int n)
 {
 	int	count;
+	int	err;
 
 	count = 0;
 	if (n < 10)
-		count += pf_putchar('0' + n);
+	{
+		count += check_err(pf_putchar('0' + n), &err);
+		if (err)
+			return (-1);
+	}
 	else
 	{
-		count += pf_putunbr(n / 10);
-		count += pf_putchar('0' + n % 10);
+		count += check_err(pf_putunbr(n / 10), &err);
+		if (err)
+			return (-1);
+		count += check_err(pf_putchar('0' + n % 10), &err);
+		if (err)
+			return (-1);
 	}
 	return (count);
 }
