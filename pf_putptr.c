588104302a1ad64f16d940511d36e85db4927629
Daniel Mateos Labrador <daniel.mateos@asafedigital.com>
2024-05-17 17:23:03 +0200

    Fix putptr strict errors

--- pf_putptr.c
+++ pf_putptr.c
@@ -6,7 +6,7 @@
 /*   By: damateos <damateos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/15 22:12:58 by damateos          #+#    #+#             */
-/*   Updated: 2024/05/17 17:16:12 by damateos         ###   ########.fr       */
+/*   Updated: 2024/05/17 17:22:29 by damateos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,16 @@ int	pf_putptr(void *ptr)
 {
 	uintptr_t	address;
 	int			count;
+	int			temp;
 
 	count = 0;
 	address = (uintptr_t)ptr;
 	count += pf_putstr("0x");
-	count += pf_puthex(address, 0);
+	if (count < 0)
+		return (-1);
+	temp = pf_puthex(address, 0);
+	if (temp < 0)
+		return (-1);
+	count += temp;
 	return (count);
 }
